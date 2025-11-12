d865f12a08f4594c5bc9804d1cc280d278358529
nipun-hub <nipunthemal@gmail.com>
2025-11-12 21:17:19 +0530

    feat( Cart ) : update item cart and add init method

--- component/ItemCartOffcanvas.js
+++ component/ItemCartOffcanvas.js
@@ -1,3 +1,5 @@
+import ItemModel from "../model/ItemModel.js";
+
 export const ItemCartOffcanvas = () => {
   return `
     <div
@@ -30,37 +32,10 @@ export const ItemCartOffcanvas = () => {
               <h5 class="fw-bold mb-0">Table 04</h5>
               <span class="text-muted">Order: #0029</span>
             </div>
-            <i class="bi bi-pencil-square fs-4 text-warning"></i>
           </div>
         </div>
 
-        <div id="cart-items" class="mb-3">
-          <div class="cart-item d-flex align-items-center mb-3">
-            <img
-              src="https://i.imgur.com/L13a0rA.png"
-              width="50"
-              height="50"
-              alt="Item"
-            />
-            <div class="ms-3 flex-grow-1">
-              <h6 class="fw-bold mb-0">Cortado</h6>
-              <span class="text-muted fs-sm">Size: large</span>
-            </div>
-            <div class="quantity-controls d-flex align-items-center">
-              <button class="btn btn-sm btn-outline-secondary btn-minus">-</button>
-              <input
-                type="text"
-                class="form-control form-control-sm text-center mx-1"
-                value="1"
-                style="width: 40px"
-              />
-              <button class="btn btn-sm btn-outline-secondary btn-plus">+</button>
-            </div>
-            <div class="ms-3 fw-bold">$8.50</div>
-            <button class="btn btn-sm btn-link text-danger ms-2">
-              <i class="bi bi-trash-fill"></i>
-            </button>
-          </div>
+        <div id="cart-items" class="mb-3" id="cart-items">
         </div>
 
         <div class="order-total">
@@ -86,9 +61,88 @@ export const ItemCartOffcanvas = () => {
         </div>
 
         <div class="d-grid gap-2 mt-4">
-          <button class="btn btn-outline-secondary">Print Invoice</button>
+          <button id="print-invoice" class="btn btn-outline-secondary">Print Invoice</button>
           <button class="btn btn-success btn-lg">Payments</button>
         </div>
       </div>
     </div>`;
+};
+
+export const loadCartItems = (cartData) => {
+  const cartDetails = cartData?.map((item) => {
+    const itemData = ItemModel.getItemById(item?.id);
+    return itemData ? { ...itemData, quantity: item?.quantity } : null;
+  });
+
+  const total = cartDetails.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  const totalItem = cartDetails.length;
+  const totalQuantity = cartDetails.reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
+
+  $("#total-items").text(totalItem);
+  $("#total-quantity").text(totalQuantity);
+
+  let cartItems = cartDetails
+    ?.map((item) => {
+      return item ? cartItemContent(item, item.quantity) : null;
+    })
+    .filter(Boolean);
+
+  if (!cartItems || cartDetails.length <= 0) {
+    cartItems = `
+      <div class="cart-item d-flex align-items-center mb-3">
+        <div class="ms-3 flex-grow-1">
+          <h6 class="fw-bold mb-0 text-secondary">No items in cart</h6>
+        </div>
+      </div>`;
+  }
+
+  $("#cart-items").html(cartItems);
+
+  $("#total-price").text(`Rs : ${total.toFixed(2)}`);
+};
+
+export const cartItemContent = (item, quantity) => {
+  return `
+     <div class="cart-item d-flex align-items-center mb-3">
+            <img
+              src="${item.image}"
+              width="50"
+              height="50"
+              alt="Item"
+            />
+            <div class="ms-3 flex-grow-1">
+              <h6 class="fw-bold mb-0">${item.name}</h6>
+            </div>
+            <div class="quantity-controls d-flex align-items-center">
+              <button class="btn btn-sm btn-outline-secondary btn-minus" data-item-id="${
+                item.id
+              }">-</button>
+              <input
+                id="quantity-input"
+                type="text"
+                class="form-control form-control-sm text-center  mx-1"
+                value="${quantity}"
+                style="width: 40px"
+              />
+              <button class="btn btn-sm btn-outline-secondary btn-plus" data-item-id="${
+                item.id
+              }">+</button>
+            </div>
+            <div class="ms-3 fw-bold">$${(item.price * quantity).toFixed(
+              2
+            )}</div>
+            <button class="btn btn-sm btn-link text-danger ms-2" id="remove-item" data-item-id="${
+              item.id
+            }">
+              <i class="bi bi-trash-fill"></i>
+            </button>
+          </div>
+    `;
 };
