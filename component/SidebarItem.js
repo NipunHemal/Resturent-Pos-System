541b1734249316094cb2d4d3c39af4feacbe215e
nipun-hub <nipunthemal@gmail.com>
2025-11-12 23:56:46 +0530

    feat( side bar ) : add side bar ictm data category name

--- component/SidebarItem.js
+++ component/SidebarItem.js
@@ -1,6 +1,6 @@
 export const SidebarContent = (item) => {
   return `
-          <a class="nav-link" href="#" title="${item.name}">
+          <a class="nav-link" href="#" title="${item.name}" data-category="${item.name}">
             <i class="${item.icon} fs-3"></i>
             <div class="d-block d-md-none d-lg-block">${item.name}</div>
           </a>
