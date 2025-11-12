a44532d3232624ea566f3ee625b2775bf476e583
nipun-hub <nipunthemal@gmail.com>
2025-11-12 21:16:15 +0530

    feat(item card) : item card add to object data

--- component/ItemCard.js
+++ component/ItemCard.js
@@ -1,14 +1,14 @@
 export const ItemCard = (item) => {
   return `
     <div class="card product-card shadow-sm " style="width: 200px; min-width: 200px;">
-      <img src="https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png" class="card-img-top product-img" alt="Grill Sandwich">
+      <img src="${item.image}" class="card-img-top product-img" alt="${item.name}">
       <div class="card-body">
         <h5 class="card-title d-flex justify-content-between align-items-center fs-6">
-          Grill Sandwich
-          <span class="text-success fs-6">$30.00</span>
+          <span>${item.name}</span>
+          <span class="text-success fs-6">$${item.price}</span>
         </h5>
         <div class="d-flex justify-content-center align-items-center mt-3">
-          <button class="btn btn-warning btn-sm">+ Add Product</button>
+          <button class="btn btn-warning btn-sm" data-item-id="${item.id}" id="add-product-to-cart">+ Add Product</button>
         </div>
       </div>
     </div>  
