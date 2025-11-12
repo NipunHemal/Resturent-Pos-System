8c76a3b4b0118a348e354c4453ea60414f8323cc
nipun-hub <nipunthemal@gmail.com>
2025-11-12 21:20:37 +0530

    feat(db) : update db file

--- db/db.js
+++ db/db.js
@@ -7,32 +7,116 @@ const userDb = {
 
 const itemDb = {
   Coffee: [
-    { id: 1, name: "Espresso", price: 3.0, quantity: 50 },
-    { id: 2, name: "Cappuccino", price: 4.0, quantity: 50 },
-    { id: 3, name: "Latte", price: 4.5, quantity: 50 },
+    {
+      id: 1,
+      name: "Espresso",
+      price: 3.0,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 2,
+      name: "Cappuccino",
+      price: 4.0,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 3,
+      name: "Latte",
+      price: 4.5,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
   ],
   Beverage: [
-    { id: 4, name: "Green Tea", price: 2.5, quantity: 50 },
-    { id: 5, name: "Black Tea", price: 2.0, quantity: 50 },
-    { id: 6, name: "Herbal Tea", price: 3.0, quantity: 50 },
+    {
+      id: 4,
+      name: "Green Tea",
+      price: 2.5,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 5,
+      name: "Black Tea",
+      price: 2.0,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 6,
+      name: "Herbal Tea",
+      price: 3.0,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
   ],
   Food: [
-    { id: 7, name: "Croissant", price: 2.5, quantity: 50 },
-    { id: 8, name: "Muffin", price: 2.0, quantity: 50 },
-    { id: 9, name: "Bagel", price: 1.5, quantity: 50 },
+    {
+      id: 7,
+      name: "Croissant",
+      price: 2.5,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 8,
+      name: "Muffin",
+      price: 2.0,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 9,
+      name: "Bagel",
+      price: 1.5,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
   ],
   Dessert: [
-    { id: 10, name: "Ice Cream", price: 2.5, quantity: 50 },
-    { id: 11, name: "Cake", price: 3.0, quantity: 50 },
-    { id: 12, name: "Donut", price: 1.5, quantity: 50 },
+    {
+      id: 10,
+      name: "Ice Cream",
+      price: 2.5,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 11,
+      name: "Cake",
+      price: 3.0,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
+    {
+      id: 12,
+      name: "Donut",
+      price: 1.5,
+      quantity: 50,
+      image:
+        "https://e7.pngegg.com/pngimages/692/99/png-clipart-delicious-food-food-salad-thumbnail.png",
+    },
   ],
 };
 
 const itemCategoryDb = [
-  { id: 1, name: "Coffee" },
-  { id: 2, name: "Beverage" },
-  { id: 3, name: "Food" },
-  { id: 4, name: "Dessert" },
+  { id: 1, name: "Coffee", icon: "bi bi-cup-hot" },
+  { id: 2, name: "Beverage", icon: "bi bi-cup-straw" },
+  { id: 3, name: "Food", icon: "bi bi-basket" },
+  { id: 4, name: "Dessert", icon: "bi bi-ice-cream" },
 ];
 
 const customerDb = [
@@ -73,6 +157,12 @@ const customerDb = [
   },
 ];
 
+const cartDb = [
+  { id: 1, quantity: 5 },
+  { id: 2, quantity: 10 },
+  { id: 3, quantity: 7 },
+];
+
 const orderDb = [
   {
     id: 1,
@@ -100,4 +190,4 @@ const orderDb = [
   },
 ];
 
-export { userDb, itemDb, itemCategoryDb, customerDb, orderDb };
+export { userDb, itemDb, cartDb, itemCategoryDb, customerDb, orderDb };
