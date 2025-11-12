21053dfca3faf81b9c368a71917009c4f8173556
nipun-hub <nipunthemal@gmail.com>
2025-11-12 21:17:53 +0530

    feat ( sidebar Card ) : create side bar card and add to object

---
+++ component/SidebarItem.js
@@ -0,0 +1,8 @@
+export const SidebarContent = (item) => {
+  return `
+          <a class="nav-link" href="#" title="${item.name}">
+            <i class="${item.icon} fs-3"></i>
+            <div class="d-block d-md-none d-lg-block">${item.name}</div>
+          </a>
+        `;
+};
