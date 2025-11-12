3f9041a32583a750d090bd7e7f0d86e831f49f47
nipun-hub <nipunthemal@gmail.com>
2025-11-12 21:15:49 +0530

    feat( script ) : clear to sctipt filr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
+import { ItemCartOffcanvas } from "../../component/ItemCartOffcanvas.js";
 import { loader } from "../../component/Loader.js";
 import { redirectToLogin } from "../../util/siteUtil.js";
-import { tost } from "../../util/tostUtil.js";
-// import { redirectToLogin } from "../../util/siteUtil.js";
+import { DashboardController } from "../../controller/DashboaedController.js";
 
 $(window).on("load", function () {
   loader();
   redirectToLogin();
 });
+
+$("body").append(ItemCartOffcanvas());
+
+new DashboardController().init();
