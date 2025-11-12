ee30ea1c024515b240e4cebc1950a6b94dc7f394
nipun-hub <nipunthemal@gmail.com>
2025-11-12 23:53:53 +0530

    feat ( script js  ) : add append order cinfirm model

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ import { ItemCartOffcanvas } from "../../component/ItemCartOffcanvas.js";
 import { loader } from "../../component/Loader.js";
 import { redirectToLogin } from "../../util/siteUtil.js";
 import { DashboardController } from "../../controller/DashboaedController.js";
+import { ConfirmOrderModel } from "../../component/ConfirmOrderModel.js";
 
 $(window).on("load", function () {
   loader();
@@ -9,5 +10,6 @@ $(window).on("load", function () {
 });
 
 $("body").append(ItemCartOffcanvas());
+$("body").append(ConfirmOrderModel.render());
 
 new DashboardController().init();
